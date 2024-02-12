917b420fe9884e2b13c2a9bb62c5861b5cf427b2
Daniel Watson <d.m.watson1985@gmail.com>
2024-02-12 14:54:38 +0100

    public func linkGoogleAccount(_ idToken: String, _ accessToken: String) async throws -> FirebaseUser {

--- Sources/FirebaseiOSMisebox/Managers/AuthenticationManager.swift
+++ Sources/FirebaseiOSMisebox/Managers/AuthenticationManager.swift
@@ -56,30 +56,19 @@ public class AuthenticationManager: ObservableObject {
 extension AuthenticationManager {
     
     @discardableResult
-       func signInWithGoogle(tokens: GoogleSignInResultModel) async throws -> FirebaseUser {
-           let credential = GoogleAuthProvider.credential(withIDToken: tokens.idToken, accessToken: tokens.accessToken)
-           let authResult = try await Auth.auth().signIn(with: credential)
-           return FirebaseUser(user: authResult.user)
-       }
-       
+    func signInWithGoogle(tokens: GoogleSignInResultModel) async throws -> FirebaseUser {
+        let credential = GoogleAuthProvider.credential(withIDToken: tokens.idToken, accessToken: tokens.accessToken)
+        let authResult = try await Auth.auth().signIn(with: credential)
+        return FirebaseUser(user: authResult.user)
+    }
+    
     @discardableResult
-    public func linkAccount(method: AuthenticationMethod) async throws -> FirebaseUser {
+    public func linkGoogleAccount(_ idToken: String, _ accessToken: String) async throws -> FirebaseUser {
         guard let currentUser = Auth.auth().currentUser else {
             throw NSError(domain: "AuthenticationManager", code: 0, userInfo: [NSLocalizedDescriptionKey: "No current user available for linking."])
         }
         
-        let credential: AuthCredential
-        switch method {
-        case .email(let email, let password):
-            credential = EmailAuthProvider.credential(withEmail: email, password: password)
-        case .google(let idToken, let accessToken):
-            credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
-        case .apple(let idToken, let rawNonce):
-            credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idToken, rawNonce: rawNonce)
-        case .anon, .other(_):
-            throw NSError(domain: "AuthenticationManager", code: -1, userInfo: [NSLocalizedDescriptionKey: "Unsupported method for linking."])
-        }
-        
+        let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
         let authResult = try await currentUser.link(with: credential)
         return FirebaseUser(user: authResult.user)
     }
@@ -90,10 +79,10 @@ extension AuthenticationManager {
     
     public enum AuthenticationMethod {
         case anon
-        case email(String, String) // Email, Password
-        case google(String, String) // ID Token, Access Token
-        case apple(String, String) // ID Token, Raw Nonce
-        case other(String) // For extending with other providers in the future
+        case email
+        case google
+        case apple
+        case other
     }
     
     public struct GoogleSignInResultModel {
